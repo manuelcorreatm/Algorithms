75a827334b93d719168acefb346d6e496963b0f3
manuelcorreatm <mjcorrea@tekmexico.com>
2016-04-16 23:32:09 -0700

    Make sure a sequence of words follow some given pattern

---
+++ tokenWordPattern1.js
@@ -0,0 +1,22 @@
+﻿//Is wrong because it is not supposed to accept same value for different token
+function isFollowingPattern(pattern, str) {
+    var words = str.split(" ");
+    obj = {};
+    if (pattern.length != words.length) return false;
+    for (var i = 0; i < pattern.length; i++) {
+        if (obj[pattern[i]]) {
+            if (obj[pattern[i]] != words[i]) {
+                return false;
+            }
+        } else {
+            obj[pattern[i]] = words[i];
+        }
+    }
+    return true;
+}
+
+
+var pattern = "aaab";
+var str = "cat cat cat hat";
+
+console.log(isFollowingPattern(pattern, str));
---
+++ tokenWordPattern2.js
@@ -0,0 +1,21 @@
+﻿//Added a second object to hold the unique values of the string;
+function isFollowingPattern(pattern, str) {
+    var words = str.split(" ");
+    tokens = {};
+    values = {};
+    if (pattern.length != words.length) return false;
+    for (var i = 0; i < pattern.length; i++) {
+        if (!tokens[pattern[i]] && !values[words[i]]) {
+            tokens[pattern[i]] = words[i];
+            values[words[i]] = pattern[i];
+        } else if (tokens[pattern[i]] != words[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+var pattern = "abba";
+var str = "cat hat hat cat";
+
+console.log(isFollowingPattern(pattern, str));
