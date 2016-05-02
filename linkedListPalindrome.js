319562cd670e56f837db17730fcee7380022577c
manuelcorreatm <mjcorrea@tekmexico.com>
2016-05-01 18:58:38 -0700

    LinkedList Palindrome
    Check if the items on a linked list form a palindrome

---
+++ linkedListPalindrome.js
@@ -0,0 +1,65 @@
+﻿/* LINKED LIST CODE */
+function LinkedList(root) {
+    this.head = new Node();
+    this.pointer = this.head;
+    this.addNode(root);
+}
+
+function Node(data) {
+    this.data = data;
+    this.next = null;
+}
+
+LinkedList.prototype.hasNext = function () {
+    return this.pointer.next ? true : false;
+};
+
+LinkedList.prototype.traverse = function (callback) {
+    this.pointer = this.head;
+    while (this.hasNext()) {
+        this.pointer = this.pointer.next;
+        callback(this.pointer);
+    }
+};
+
+LinkedList.prototype.addNode = function (node) {
+    this.pointer = this.head;
+    while (this.hasNext()) {
+        this.pointer = this.pointer.next;
+    }
+    this.pointer.next = node;
+};
+
+/* PALINDROME CODE */
+LinkedList.prototype.isPalindrome = function () {
+    this.pointer = this.head;
+    var word = [];
+    var isit = true;
+    function check(pointer) {
+        if (pointer.next) {
+            word.push(pointer.next.data);
+            //console.log(word);
+            check(pointer.next);
+        }
+        if (pointer.data != word.shift()) {
+            isit = false;
+        }
+    }
+
+    check(this.pointer);
+    return isit;
+};
+
+/* TEST CASE */
+var node1 = new Node(1);
+var node2 = new Node(3);
+var node3 = new Node(5);
+var node4 = new Node(3);
+var node5 = new Node(1);
+var llist = new LinkedList(node1);
+llist.addNode(node2);
+llist.addNode(node3);
+llist.addNode(node4);
+llist.addNode(node5);
+
+console.log(llist.isPalindrome());
---
+++ maximize.js
@@ -0,0 +1,20 @@
+﻿function maximize(target) {
+    if (target <= 1) return 0;
+    if (target == 2) return 1;
+    if (target == 3) return 2;
+    var triples = Math.floor(target / 3);
+    remainder = target % 3;
+
+    if (remainder == 1) {
+        --triples;
+        remainder += 3;
+    }
+
+    if (remainder === 0) {
+        return Math.pow(3, triples);
+    } else {
+        return Math.pow(3, triples) * remainder;
+    }
+}
+
+console.log(maximize(20));
